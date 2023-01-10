6fc13f203c76de9b8855bbeee8a12c15c828c168
shubham kumar <devshubhamyadav@gmail.com>
2023-01-10 04:43:57 +0000

    shubham

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,5 +1,6 @@
 // models
 const Post = require("../../models/Post.js");
+const checkAuth = require("../../utils/check-auth.js");
 module.exports = {
   Query: {
     async getPosts() {
@@ -10,16 +11,42 @@ module.exports = {
         throw new Error(error);
       }
     },
-    async getPost(_,{postId}){
+    async getPost(_, { postId }) {
       try {
         const existingPost = await Post.findById(postId);
         if (existingPost) {
-          
+          return existingPost;
+        } else {
+          throw new Error("Post Not Found!");
         }
-        
       } catch (error) {
-        
+        throw new Error(error);
       }
-    }
+    },
+  },
+  Mutation: {
+    async createPost(_, { body }, context) {
+      try {
+        const user = checkAuth(context);
+        const newPost = new Post({
+          body,
+          user: user.indexOf,
+          username: user.username,
+          createdAt: new Date().toISOString(),
+        });
+
+        const post = await newPost.save();
+        return post;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    async deletePost(_, { postId }, context) {
+      try {
+        const user = checkAuth(context);
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
   },
 };
--- graphql/resolvers/resolver.js
+++ graphql/resolvers/resolver.js
@@ -6,5 +6,6 @@ module.exports = {
   },
   Mutation: {
     ...userResolvers.Mutation,
+    ...postResolvers.Mutation,
   },
 };
